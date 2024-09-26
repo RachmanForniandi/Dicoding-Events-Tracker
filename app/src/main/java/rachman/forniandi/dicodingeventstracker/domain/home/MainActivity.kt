2ee526b3c72a795569813bd6f195b2247825b739
RachmanForniandi <rachmanforniandi@gmail.com>
2024-09-26 15:39:27 +0700

    removed unnecessary set action bar on Main Activity page. Set Theme on Main activity.

--- app/src/main/java/rachman/forniandi/dicodingeventstracker/domain/home/MainActivity.kt
+++ app/src/main/java/rachman/forniandi/dicodingeventstracker/domain/home/MainActivity.kt
@@ -1,24 +1,16 @@
 package rachman.forniandi.dicodingeventstracker.domain.home
 
-import android.content.Intent
+
 import android.os.Bundle
-import android.provider.Settings
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import rachman.forniandi.dicodingeventstracker.R
 import rachman.forniandi.dicodingeventstracker.databinding.ActivityMainBinding
-import rachman.forniandi.dicodingeventstracker.domain.search.SearchEventFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -28,27 +20,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Base_Theme_DicodingEventsTracker)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.mainAppBar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        //val navBottomView = binding.bottomNavView
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_container) as NavHostFragment
         navController = navHostFragment.navController
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.homeFragment,
-                R.id.upcomingEventsFragment,
-                R.id.pastEventsFragment,
-                R.id.favoriteEventsFragment
-            )
-        )
         binding.bottomNavView.setupWithNavController(navController)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navController.addOnDestinationChangedListener{ _, destination, _ ->
             when (destination.id) {
                 R.id.homeFragment -> binding.bottomNavView.visibility = View.VISIBLE
@@ -58,12 +40,7 @@ class MainActivity : AppCompatActivity() {
                 else -> binding.bottomNavView.visibility = View.GONE
             }
         }
-
-
     }
-
-
-
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp()|| super.onSupportNavigateUp()
