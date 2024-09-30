aad51cac609dadf6b8c12e9e50991aafc6c50cb6
RachmanForniandi <rachmanforniandi@gmail.com>
2024-09-30 15:40:49 +0700

    change theme app to night theme using datastore works functionally but still not match combination color theme yet .

--- app/src/main/java/rachman/forniandi/dicodingeventstracker/domain/home/MainActivity.kt
+++ app/src/main/java/rachman/forniandi/dicodingeventstracker/domain/home/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Base_Theme_DicodingEventsTracker)
+        setTheme(R.style.Theme_DicodingEventsTracker)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
